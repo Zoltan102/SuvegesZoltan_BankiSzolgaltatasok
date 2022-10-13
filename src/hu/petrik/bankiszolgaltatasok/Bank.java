2a903b4a32c4f9051af77b1aeab7a9b442ba2eb2
Zoli <suveges.zoltan@petrik.hu>
2022-10-13 10:10:25 +0200

    Bank +minden kész

--- src/hu/petrik/bankiszolgaltatasok/Bank.java
+++ src/hu/petrik/bankiszolgaltatasok/Bank.java
@@ -1,31 +1,53 @@
 package hu.petrik.bankiszolgaltatasok;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Bank {
     private List<Szamla> szamlaLista;
 
     public Bank(List<Szamla> szamlaLista) {
+        this.szamlaLista = new ArrayList<>();
         this.szamlaLista = szamlaLista;
     }
 
     public Szamla szamlaNyitas(Tulajdonos tulajdonos, int hitelKeret) {
-        //TODO: szamlanyitas
-        return null;
+        Szamla ujSzamla;
+        if (hitelKeret == 0) {
+            ujSzamla = new MegtakaritasiSzamla(tulajdonos);
+        } else {
+            ujSzamla = new HitelSzamla(tulajdonos, hitelKeret);
+        }
+        return ujSzamla;
     }
 
     public int getOsszegyenleg(Tulajdonos tulajdonos) {
-        //TODO: getOsszegyenleg
-        return 0;
+        int osszegyenleg = 0;
+        for (Szamla sz : szamlaLista) {
+            if (sz.getTulajdonos() == tulajdonos) {
+                osszegyenleg += sz.getAktualisEgyenleg();
+            }
+        }
+        return osszegyenleg;
     }
 
     public Szamla getLegnagyobbEgyenleguSzamla(Tulajdonos tulajdonos) {
-        return null;
-        //TODO: getLegnagyobbEgyenleguSzamla
+        Szamla legnagyobbSzamla = new MegtakaritasiSzamla(tulajdonos);
+        for (Szamla sz : szamlaLista) {
+            if (tulajdonos == sz.getTulajdonos() && legnagyobbSzamla.aktualisEgyenleg < sz.aktualisEgyenleg) {
+                legnagyobbSzamla = sz;
+            }
+        }
+        return legnagyobbSzamla;
     }
 
     public long getOsszHitelkeret() {
-        return 0;
-        //TODO: getOsszHitelkeret
+        long osszHitelkeret = 0;
+        for (Szamla sz : szamlaLista) {
+            if(sz.getClass() == HitelSzamla.class){
+                osszHitelkeret += ((HitelSzamla) sz).getHitelKeret();
+            }
+        }
+        return osszHitelkeret;
     }
 }
