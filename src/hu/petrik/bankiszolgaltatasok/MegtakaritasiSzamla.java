dc8e1cf46de7b042a0f35a4d6b9066516be565db
Zoli <suveges.zoltan@petrik.hu>
2022-10-13 09:18:34 +0200

    MegtakaritasiSzamla +kivesz kész +kamatJovairas kész

--- src/hu/petrik/bankiszolgaltatasok/MegtakaritasiSzamla.java
+++ src/hu/petrik/bankiszolgaltatasok/MegtakaritasiSzamla.java
@@ -1,10 +1,12 @@
 package hu.petrik.bankiszolgaltatasok;
 
-public class MegtakaritasiSzamla {
+public class MegtakaritasiSzamla extends Szamla {
     private double kamat;
     public static final double alapKamat = 1.1;
 
     public MegtakaritasiSzamla(Tulajdonos tulajdonos) {
+        super(tulajdonos);
+        kamat = alapKamat;
     }
 
     public double getKamat() {
@@ -16,11 +18,15 @@ public class MegtakaritasiSzamla {
     }
 
     public boolean kivesz(int osszeg) {
-        return true;
-        //TODO: kivesz
+        boolean sikeresKivetel = false;
+        if (aktualisEgyenleg - osszeg >= 0) {
+            aktualisEgyenleg -= osszeg;
+            sikeresKivetel = true;
+        }
+        return sikeresKivetel;
     }
 
     public void kamatJovairas() {
-        //TODO: kamatJovairas
+        aktualisEgyenleg = (int) (kamat * aktualisEgyenleg);
     }
 }
